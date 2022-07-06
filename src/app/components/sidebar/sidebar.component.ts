11cf0230d17433bb8546c9f23d606cecd1a3b9c1
batool Al-omari <bomari@intracom.jo>
2022-07-06 12:09:10 +0300

    save changes

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -10,9 +10,9 @@ declare interface RouteInfo {
 export const ROUTES: RouteInfo[] = [
     { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
     { path: '/user-profile', title: 'User Profile',  icon:'person', class: '' },
-    { path: '/table-list', title: 'Table List',  icon:'content_paste', class: '' },
-    { path: '/typography', title: 'Typography',  icon:'library_books', class: '' },
-    { path: '/maps', title: 'Maps',  icon:'location_on', class: '' },    
+    { path: '/table-list', title: 'Products',  icon:'content_paste', class: '' },
+    { path: '/typography', title: 'Branches',  icon:'library_books', class: '' },
+    { path: '/maps', title: 'Head Office Location',  icon:'location_on', class: '' },    
     { path: '/auth/login', title: 'Logout',  icon:'login', class: '' },
 ];
 
--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -2,8 +2,8 @@ import { Routes } from '@angular/router';
 
 import { DashboardComponent } from '../../dashboard/dashboard.component';
 import { UserProfileComponent } from '../../user-profile/user-profile.component';
-import { TableListComponent } from '../../table-list/table-list.component';
-import { TypographyComponent } from '../../typography/typography.component';
+import { TableListComponent } from '../../Products/table-list.component';
+import { TypographyComponent } from '../../branches/typography.component';
 import { MapsComponent } from '../../maps/maps.component';
 import { AdminLayoutComponent } from './admin-layout.component';
 
